417f89a5799f50a4392ed29569377187e37ad766
Josh Riddle <riddlej9879@yahoo.com>
2021-05-16 08:51:01 -0500

    Section3 Exercise 3 and 4 completed

--- Section3/Exercise4.py
+++ Section3/Exercise4.py
@@ -1,14 +1,22 @@
 class Node:
-    # insert the def __init__(self, ...)
     def __init__(self, element=None, next_node=None):
         self.element = element
         self.next_node = next_node
 
 
+def print_list(n):
+    # insert code here
+    while n is not None:
+        # use print(n.element, ' ', end='')
+        print(n.element, ' ', end='')
+        n = n.next_node
+    # rember to add a new line with print()
+    print()
+
+
 def linked_list():
     l = Node(5, Node(3, Node(13, Node(4, Node(30)))))
-    print(l.element, l.next_node.element, l.next_node.next_node.element, l.next_node.next_node.next_node.element,
-          l.next_node.next_node.next_node.next_node.element)
+    print_list(l)
 
 
 if __name__ == "__main__":
